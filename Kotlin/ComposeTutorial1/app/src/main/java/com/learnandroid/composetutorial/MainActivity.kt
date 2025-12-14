1adaf1c110bab8e1283732f193bc5d98e7e6e91c
JESDO525 <bernardojgm05@gmail.com>
2025-12-13 20:18:46 -0600

    feat(MainActivity): Carta de perfil con imagen

--- Kotlin/ComposeTutorial1/app/src/main/java/com/learnandroid/composetutorial/MainActivity.kt
+++ Kotlin/ComposeTutorial1/app/src/main/java/com/learnandroid/composetutorial/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.learnandroid.composetutorial.ui.theme.ComposeTutorial1Theme
 
@@ -18,17 +22,31 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Text("Hello World")
+            MesseageCard(Message("Android", "Jetpack Compose"))
         }
     }
 }
+data class Message(val author: String, val body: String)
+
 @Composable
-fun MesseageCard(name: String){
-    Text(text = "Hello $name")
+fun MesseageCard(msg: Message){
+    Row(){
+        Image(
+            painter = painterResource(R.drawable.finn),
+            contentDescription = "Contact profile picture"
+        )
+        Column() {
+            Text(text = msg.author)
+            Text(text = msg.body)
+        }
+    }
+
 }
 
 @Preview
 @Composable
 fun PreviewMesseageCard(){
-    MesseageCard(name = "Bernardo")
+    MesseageCard(
+        msg = Message("Finn", "Hey, take a look at Jetpack Compose, its great!")
+    )
 }
