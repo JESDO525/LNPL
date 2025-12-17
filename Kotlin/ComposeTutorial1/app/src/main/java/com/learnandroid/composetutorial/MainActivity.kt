a73283adab262e53a41699fb12015e996a55a274
JESDO525 <bernardojgm05@gmail.com>
2025-12-16 21:42:05 -0600

    feat: Codigo documentado

--- Kotlin/ComposeTutorial1/app/src/main/java/com/learnandroid/composetutorial/MainActivity.kt
+++ Kotlin/ComposeTutorial1/app/src/main/java/com/learnandroid/composetutorial/MainActivity.kt
@@ -1,5 +1,4 @@
 package com.learnandroid.composetutorial
-
 import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -37,6 +36,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.learnandroid.composetutorial.ui.theme.ComposeTutorial1Theme
 
+/**
+ * Actividad principal de la aplicación.
+ */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,31 +51,35 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+/**
+ * Representa un mensaje en la conversación.
+ *
+ * @param author El autor del mensaje.
+ * @param body El cuerpo del mensaje.
+ */
 data class Message(val author: String, val body: String)
 
+/**
+ * Muestra una tarjeta con un mensaje.
+ *
+ * @param msg El mensaje a mostrar.
+ */
 @Composable
 fun MesseageCard(msg: Message){
-    //I need to add  padding around our message
     Row(modifier = Modifier.padding(all = 8.dp)){
         Image(
             painter = painterResource(R.drawable.selection),
             contentDescription = null,
             modifier = Modifier
-                //Set the image size to 40 dp
                 .size(40.dp)
-                //Clip the image to be shaped as a cricle
                 .clip(CircleShape)
                 .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
         )
         Spacer(modifier = Modifier.width(8.dp))
-        //We keep track if the message is expanded or not in this variable
         var isExpanded by remember{ mutableStateOf(false) }
-        //SurfaceColor will be updated gradually from one color to the other
         val surfaceColor by animateColorAsState(
             if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
         )
-
-        //We toggle the isExpanded variable when we click on this Column
         Column(modifier = Modifier.clickable { isExpanded = !isExpanded }){
             Text(
                 text = msg.author,
@@ -81,20 +87,15 @@ fun MesseageCard(msg: Message){
                 style = MaterialTheme.typography.titleSmall
             )
             Spacer(modifier = Modifier.height(4.dp))
-
             Surface(
                 shape = MaterialTheme.shapes.medium,
                 shadowElevation = 1.dp,
-                //surfaceColor color will be changing gradually from primary to surface
                 color = surfaceColor,
-                //animateContentSize will change the Surface size gradually
                 modifier = Modifier.animateContentSize().padding(1.dp)
             ) {
                 Text(
                     text = msg.body,
                     modifier = Modifier.padding(all = 4.dp),
-                    //if the message is expanded, we display all its content
-                    //otherwise we only display the first line
                     maxLines = if (isExpanded) Int. MAX_VALUE else 1,
                     style = MaterialTheme.typography.bodyMedium
                 )
@@ -103,6 +104,11 @@ fun MesseageCard(msg: Message){
     }
 }
 
+/**
+ * Muestra una lista de mensajes en una conversación.
+ *
+ * @param messages La lista de mensajes a mostrar.
+ */
 @Composable
 fun Conversation(messages: List<Message>){
     LazyColumn{
@@ -112,7 +118,9 @@ fun Conversation(messages: List<Message>){
     }
 }
 
-//Preview Normal sin Modo claro o oscuro
+/**
+ * Vista previa de la tarjeta de mensaje.
+ */
 @Preview
 @Composable
 fun PreviewMesseageCard1(){
@@ -121,6 +129,9 @@ fun PreviewMesseageCard1(){
     )
 }
 
+/**
+ * Vista previa de la conversación.
+ */
 @Preview
 @Composable
 fun PreviewConversation(){
@@ -128,22 +139,16 @@ fun PreviewConversation(){
         Conversation(SampleData.conversationSample)
     }
 }
+
 /**
- * A Composable function that previews the `MesseageCard` component.
- * It's annotated with `@Preview` to be visible in Android Studio's preview pane.
- * This function sets up two previews: one for "Light Mode" and another for "Dark Mode",
- * allowing for easy visual testing of the UI in different theme configurations.
- * It wraps the `MesseageCard` in a `ComposeTutorial1Theme` and a `Surface` to provide
- * the necessary theming and background.
+ * Vista previa de la tarjeta de mensaje en modo claro y oscuro.
  */
-//Preview con Modo Claro y Oscuro
 @Preview(name = "Light Mode")
 @Preview(
     uiMode = Configuration.UI_MODE_NIGHT_YES,
     showBackground = true,
     name = "Dark Mode"
 )
-
 @Composable
 fun PreviewMessageCard2() {
     ComposeTutorial1Theme{
